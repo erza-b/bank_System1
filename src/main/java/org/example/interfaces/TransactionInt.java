e5a9707674957fd758b37d86f7d39fa0c2d6c8cd
Erza Bërbatovci <116487812+erza-b@users.noreply.github.com>
2024-05-18 02:46:21 +0200

    Implement Transaction class with basic functionality

--- src/main/java/org/example/interfaces/TransactionInt.java
+++ src/main/java/org/example/interfaces/TransactionInt.java
@@ -1,4 +1,7 @@
 package org.example.interfaces;
 
 public interface TransactionInt {
+    double calculateFee(double amount, double flatFee, double percentFee, int feeType);
+
+    String getTransactionType();
 }
--- src/main/java/org/example/model/Transaction.java
+++ src/main/java/org/example/model/Transaction.java
@@ -1,4 +1,74 @@
 package org.example.model;
 
-public class Transaction {
+import org.example.interfaces.TransactionInt;
+
+public class Transaction implements TransactionInt {
+
+    private double transactionAmount;
+    private int senderAccountId;
+    private int receiverAccountId;
+    private String reason;
+    private double transactionFee;
+
+    public Transaction(double transactionAmount, int senderAccountId, int receiverAccountId, String reason, double transactionFee) {
+        this.transactionAmount = transactionAmount;
+        this.senderAccountId = senderAccountId;
+        this.receiverAccountId = receiverAccountId;
+        this.reason = reason;
+        this.transactionFee = transactionFee;
+    }
+
+    public double getTransactionAmount() {
+        return transactionAmount;
+    }
+
+    public void setTransactionAmount(double transactionAmount) {
+        this.transactionAmount = transactionAmount;
+    }
+
+    public double getTransactionFee() {
+        return transactionFee;
+    }
+
+    public void setTransactionFee(double transactionFee) {
+        this.transactionFee = transactionFee;
+    }
+
+    public int getSenderAccountId() {
+        return senderAccountId;
+    }
+
+    public void setSenderAccountId(int senderAccountId) {
+        this.senderAccountId = senderAccountId;
+    }
+
+    public int getReceiverAccountId() {
+        return receiverAccountId;
+    }
+
+    public void setReceiverAccountId(int receiverAccountId) {
+        this.receiverAccountId = receiverAccountId;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    @Override
+    public double calculateFee(double amount, double flatFee, double percentFee, int feeType) {
+       return 1;
+    }
+
+    @Override
+    public String getTransactionType() {
+        return "Standard Transaction";
+    }
+
+    public static boolean isTransactionPossible(double balance, double amount) {
+        return balance >= amount;
+    }
 }
