e9e312eaf1c48d1aa6e7294c432629d11f699b92
Erza Bërbatovci <116487812+erza-b@users.noreply.github.com>
2024-05-19 13:43:41 +0200

    configure security settings to allow open access to account endpoints

--- src/main/java/org/example/config/securityConfiguration.java
+++ src/main/java/org/example/config/securityConfiguration.java
@@ -1,4 +1,21 @@
 package org.example.config;
 
-public class securityConfiguration {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class securityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/api/accounts").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/accounts").permitAll()
+                .anyRequest().authenticated();
+    }
 }
