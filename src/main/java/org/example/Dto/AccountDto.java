82f5c326930ca97bc97aa9d37069139581115f48
Erza Bërbatovci <116487812+erza-b@users.noreply.github.com>
2024-05-19 02:03:21 +0200

    Implemented AccountDto

--- src/main/java/org/example/Dto/AccountDto.java
+++ src/main/java/org/example/Dto/AccountDto.java
@@ -1,4 +1,32 @@
 package org.example.Dto;
 
 public class AccountDto {
+    private String name;
+    private double balance;
+    public int accountId;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
 }
+
