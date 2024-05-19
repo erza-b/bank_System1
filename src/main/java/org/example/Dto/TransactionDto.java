6ab9ed4d2dc095c745b3e51572c24e8085b73b73
Erza Bërbatovci <116487812+erza-b@users.noreply.github.com>
2024-05-19 02:04:08 +0200

    Implemented TransactionDto

--- src/main/java/org/example/Dto/TransactionDto.java
+++ src/main/java/org/example/Dto/TransactionDto.java
@@ -1,4 +1,41 @@
 package org.example.Dto;
 
 public class TransactionDto {
+    private double amount;
+    private int originatingAccountId;
+    private int resultingAccountId;
+    private String transactionReason;
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public int getOriginatingAccountId() {
+        return originatingAccountId;
+    }
+
+    public void setOriginatingAccountId(int originatingAccountId) {
+        this.originatingAccountId = originatingAccountId;
+    }
+
+    public int getResultingAccountId() {
+        return resultingAccountId;
+    }
+
+    public void setResultingAccountId(int resultingAccountId) {
+        this.resultingAccountId = resultingAccountId;
+    }
+
+    public String getTransactionReason() {
+        return transactionReason;
+    }
+
+    public void setTransactionReason(String transactionReason) {
+        this.transactionReason = transactionReason;
+    }
 }
+
